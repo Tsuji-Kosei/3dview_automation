4f9373df5cacd396ba77031dcb0f1b5e4a71c902
Kosei <kosei@kangsheng.com>
2021-06-14 01:08:22 +0900

    info

--- 3dvista_edit_code_new/Edit_Script.py
+++ 3dvista_edit_code_new/Edit_Script.py
@@ -12,20 +12,27 @@ class Add_Json_Data():
         self.json_load = json.load(self.json_open) #3Dvistaのプロジェクトファイルの中のscript.js
         self.db_load = json.load(self.db_open) #detabase.js
         self.number_panorama = 0      #登録されている写真の枚数
-        self.connect_hotspot = []      #[[[画像名,x座標,y座標]]]の三次元
-        self.number_hotspot = []    # リスト[3,4,5] (1枚目の写真に３枚、、、)
-        self.overlays = []  #
+        self.connect_hotspot = {}      #{出発地点の画像名:[[繋がり先の画像名,x座標,y座標],[],  ,,}の辞書
+        self.connect_info = {}      #{infoが貼られる画像名:[[ローカルの画像名,x座標,y座標],[],  ,,}の辞書
+        self.number_info = {}       # 辞書(出発地点の画像名:3, ,, } (1枚目の写真に３個、、、)
+        self.number_hotspot = {}    # 辞書(infoが貼られる画像名:3, ,, } (1枚目の写真に３個、、、)
+        self.overlays = {}      #{出発地点の画像名:[overlayのid,overlayのid,,  ], } の辞書　(idは前から順番につなげる先の写真に対応)
         self.areas = []
         self.behaviours = []
         self._get_number_panorama() #　self.number_panoramaに登録されている写真の枚数を格納
         self._get_connect_hotspot() # self.conncect.hotspotに、[[[つなげる先の写真名、x,y],[] , ],[],[]]という形のリストが入っている。大きな塊は登録されている写真の順番になっている。
         self._get_number_hotspot() # self.number_hotspotに、それぞれの写真に何枚の写真が繋がっているかのリスト[3,4,5] (1枚目の写真に３枚、、、)　を格納
+        self._get_connect_info()
+        self._get_number_info() 
+        print(self.number_hotspot)
+        print(self.number_info)
+        print(self.connect_hotspot)
+        print(self.connect_info)
+        # self.remember_panorama_info() # script.jsの中のパノラマに関する情報をクラス変数に格納(詳しくはこの関数の先に書いています)
 
-        self.remember_panorama_info() # script.jsの中のパノラマに関する情報をクラス変数に格納(詳しくはこの関数の先に書いています)
-
-        self._get_overlays() #self.overlaysに、[[１枚目のパノラマのoverlay, , , ],[2枚目のパノラマのoverlay, , , ],[] ,[]]]というリストを格納
-        self._get_areas()   #self.areasに、[[１枚目のパノラマのareas, , , ],[2枚目のパノラマのareas, , , ],[] ,[]]]というリストを格納
-        self._get_behaviours() #self.behavioursに、[[１枚目のパノラマのbehaviours, , , ],[2枚目のパノラマのbehaviours, , , ],[] ,[]]]というリストを格納
+        self._get_overlays() #self.overlaysに、{出発地点の画像名:[overlayのid,overlayのid,,  ], } の辞書を格納
+        # self._get_areas()   #self.areasに、[[１枚目のパノラマのareas, , , ],[2枚目のパノラマのareas, , , ],[] ,[]]]というリストを格納
+        # self._get_behaviours() #self.behavioursに、[[１枚目のパノラマのbehaviours, , , ],[2枚目のパノラマのbehaviours, , , ],[] ,[]]]というリストを格納
         
     
     def _get_number_panorama(self):
@@ -40,25 +47,37 @@ class Add_Json_Data():
     def _get_connect_hotspot(self):
         
         for i in self.db_load.keys():
+            print(i)
             pre_connect_hotspot = []
-            for j in range(len(self.db_load[i])):
-                angle = self.db_load[i][j][1]
-                # print(self.db_load[i][j][0])
-                pre_connect_hotspot.append([self.db_load[i][j][0],700+700*angle/math.pi, angle*180/math.pi])  ##connect_hotspotに名前,x, yでappendしたい
-            self.connect_hotspot.append(pre_connect_hotspot)
-        # print(self.connect_hotspot)
+            for j in range(len(self.db_load[i]["hotspot"])):
+                angle = self.db_load[i]["hotspot"][j][1]
+                pre_connect_hotspot.append([self.db_load[i]["hotspot"][j][0],700+700*angle/math.pi, angle*180/math.pi])  ##connect_hotspotに名前,x, yでappendしたい
+            self.connect_hotspot[i]=pre_connect_hotspot
+
+    def _get_connect_info(self):
+        
+        for i in self.db_load.keys():
+            pre_connect_info = []
+            for j in range(len(self.db_load[i]["info"])):
+                pre_connect_info.append([self.db_load[i]["info"][j][0],self.db_load[i]["info"][j][1],self.db_load[i]["info"][j][2]])  ##connect_hotspotに名前,x, yでappendしたい
+            self.connect_info[i]=pre_connect_info
 
     def _get_number_hotspot(self):
-        for i in range(self.number_panorama):
-            self.number_hotspot.append(len(self.connect_hotspot[i]))
+        for i in self.connect_hotspot.keys():
+            self.number_hotspot[i]=len(self.connect_hotspot[i])
     
-    def _get_overlays(self):
-        for i in range(self.number_panorama):
+    def _get_number_info(self):
+        for i in self.connect_info.keys():
+            self.number_info[i]=len(self.connect_info[i])
+    
+    def _get_overlays(self): # 辞書の上から順に命名していく
+        count = 0
+        for i in self.number_hotspot.keys():
             for j in range(self.number_hotspot[i]):
                 if j == 0: #まずkey作成
-                    self.overlays.append({"overlays":[f"this.overlay_C{i}_{j}"]})
+                    self.overlays[i] = {"overlays":[f"this.overlay_C{count}_{j}"]}
                 else: # value追加
-                    self.overlays[i]["overlays"].append(f"this.overlay_C{i}_{j}")
+                    self.overlays[i]["overlays"].append(f"this.overlay_C{count}_{j}")
     
     def _get_areas(self):
         for i in range(self.number_panorama):
@@ -124,12 +143,12 @@ class Add_Json_Data():
                             "where":"inViewer"}]})
 
     def remember_context_panorama(self):
-        self.remember_context = [] ##今までのremember
+        self.remember_context = {}
         json_definition = self.json_load["player"]["definitions"]
         for i in range(len(json_definition)):
             for v in json_definition[i].keys():
                 if(v=='hfovMin'):
-                    self.remember_context.append(json_definition[i])
+                    self.remember_context[json_definition[i]["paths"].split(".")[0]]=json_definition[i]
 
     def remember_position_panorama(self):
         self.remember_number = []
@@ -157,14 +176,12 @@ class Add_Json_Data():
             count += 1
     
     def remember_panorama_info(self): # script.jsのplayer->definitionsの中の"hfovMin"というキーを持つvalueがパノラマに関する情報
-        self.remember_context_panorama() # "hfovMin"というキーのvalueの中身がself.remember_contextに格納されてr￥イル
+        self.remember_context_panorama() # {中身に対応する画像名:"hfovMin"というキーのvalueの中身,,,}がself.remember_contextに格納されてイル
         self.remember_position_panorama() # "hfovMin"というキーを持つ辞書が、definitionsの中の何番目にあったかを格納
         self.remember_name_and_id_panorama() # self.remember_name_idに{画像の名前(.JPG抜き): パノラマに対応したid} という辞書が格納される
 
 
     def insert_overlays(self):
-        # self.remember_context_panorama()
-        # self.remember_position_panorama()
         # overlayをそれぞれに追加
         for i in range(len(self.remember_context)):
             self.remember_context[i].update(self.overlays[i])
@@ -197,12 +214,12 @@ class Add_Json_Data():
 
 
 def main():
-    AJD = Add_Json_Data(r"C:\Users\owner\Documents\3dvista_automation\test_code\script.js", "data_base.js")
-    AJD.insert_overlays()
-    AJD.insert_areas()
-    AJD.insert_behaviours()
-    AJD.save_to_json("script.json")
-    AJD.file_close()
+    AJD = Add_Json_Data("/Users/tsujikousei/Documents/rutelia/3dview_code/3dview_automation/3dvista_edit_code_new/script.js", "database.js")
+    # AJD.insert_overlays()
+    # AJD.insert_areas()
+    # AJD.insert_behaviours()
+    # AJD.save_to_json("script.json")
+    # AJD.file_close()
 
 
 if __name__ == "__main__":
--- 3dvista_edit_code_new/create_database.py
+++ 3dvista_edit_code_new/create_database.py
@@ -1,45 +1,67 @@
 import json
 from cal_dis_angle import vincenty_inverse
 
-def create_db(original_path, data_path, distance_limit = 100000):
+def create_db(original_path, data_path):
     json_open = open(original_path,'r')
     json_load = json.load(json_open)
 
-    coordinates = json_load["markers"]
+    coordinates_info = json_load["markers"]
     line_segments = json_load["line_segments"]
     data_for_databese = {}
 
-    for origin_image in line_segments:
-        result_calculation = []
+    #　hotspotに関する情報をまとめる
+    for origin_image in line_segments: #origin_Imageは出発地点の画像の名前
+        # print(origin_image)
+
+        result_calculation = {}
+        pre_result_calculation = [] #それぞれの写真に対してのhotspotで繋がっている画像名と座標のリストのリストを格納
+        count = 0 # 次のloopで使用　リストに正しく格納するため
+
         for connected_image in line_segments[origin_image]:
-            pre_result_calculation = []
+            pre_result_calculation.append([])
             prepare_position = []
+            prepare_position.append(coordinates_info[origin_image])
+            prepare_position.append(coordinates_info[connected_image])
 
-            prepare_position.append(coordinates[origin_image])
-            prepare_position.append(coordinates[connected_image])
-
-            lat1 = prepare_position[0][0]  
+            lat1 = prepare_position[0][0]  #出発地点の写真の座標
             lon1 = prepare_position[0][1]
-            lat2 = prepare_position[1][0]
+            lat2 = prepare_position[1][0]   #つながる先の写真の座標
             lon2 = prepare_position[1][1]
 
             result = vincenty_inverse(lat1, lon1, lat2, lon2)
 
-            # print(connected_image)
-            pre_result_calculation.append(connected_image.split(".")[0])
-            pre_result_calculation.append(result['angle'])
-            result_calculation.append(pre_result_calculation)
-        # print(result_calculation)
+            pre_result_calculation[count].append(connected_image.split(".")[0])
+            pre_result_calculation[count].append(result['angle'])
+            count += 1
+
+        result_calculation["hotspot"]=pre_result_calculation
+        # dic_list =[] # infoの辞書とまとめるため急遽追加
+        # dic_list.append(result_calculation)
         data_for_databese[origin_image.split(".")[0]] = result_calculation
 
-        with open(data_path, mode='wt', encoding='utf-8') as file:
-            json.dump(data_for_databese, file, ensure_ascii=False, indent=2)
+    # infoに関する情報をまとめる
+    for attached_image in coordinates_info: # attached_Imageが貼り付ける写真の画像名
+        info_dic = {}
+        info_list = []
+        count=0
+        for number_of_info in range(len(coordinates_info[attached_image])-2):
+            info_list.append([])
+            info_list[count]= coordinates_info[attached_image][number_of_info+2] #0,1には画像自体の座標が入っている。２個目以降がinfoに関する情報
+            count +=1
+        info_dic["info"] = info_list
+        # print(info_dic)
+        # print(data_for_databese[attached_image.split(".")[0]])
+        data_for_databese[attached_image.split(".")[0]].update(info_dic)
 
-    else:
-        pass
+
+
+
+    with open(data_path, mode='wt', encoding='utf-8') as file:
+        json.dump(data_for_databese, file, ensure_ascii=False, indent=2)
+
 
 def main():
-    create_db("coordinates.json","test.js", distance_limit=1.1)
+    create_db("Images/coordinates.json","database.js")
 
 if __name__ == "__main__":
     main()
